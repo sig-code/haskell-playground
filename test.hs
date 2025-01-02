d024071046e8c292f0983c8d2e06fcc56e3a1341
Takuya Mashimo <m.takku29@gmail.com>
2025-01-02 23:04:35 +0900

    とりあえずhaskell書いてみた

---
+++ test.hs
@@ -0,0 +1,12 @@
+numbers :: [Int]
+numbers = [1, 2, 3, 4, 5]
+
+strings :: [String]
+strings = ["one", "two", "three", "four", "five"]
+
+range1 = [1..5]
+range2 = [2,4..10]
+
+combined = [1,2,3] ++ [4,5,6] -- [1,2,3,4,5,6]
+
+newList = 1 : [2,3,4] -- [1,2,3,4]
